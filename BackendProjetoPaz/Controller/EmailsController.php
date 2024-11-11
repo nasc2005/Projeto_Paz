a5436c38de03a22d87665d9ca075e79ce865fb18
nasc2005 <murilovnasc.prog@gmail.com>
2024-11-11 15:05:44 -0300

    enviando email de redefinição

--- BackendProjetoPaz/Controller/EmailsController.php
+++ BackendProjetoPaz/Controller/EmailsController.php
@@ -44,16 +44,60 @@ switch ($_SERVER['REQUEST_METHOD']) {
                                 "message" => "Email Enviado com Sucesso!", 
                             ]);
                             break;
-                    case $action == "NovoVendedor":
-                    // var_dump("deu bom");
-                    $email=  $_GET['email'];
-                    // var_dump($email);
-                    $service->EnviaEmailVendedor($email);
-                    http_response_code(200);
-                    echo json_encode([
-                    "message" => "Email Enviado com Sucesso!", 
-                    ]);
-                    break;            
+                            case $action == "FinalizarVenda":
+                                // Chama o serviço para obter os usuários com o perfil "Administrador"
+                                $result = $service->readByPerfil2("Administrador");
+                            
+                                // Verifica se a chave 'data' existe e é um array
+                                if (isset($result['data']) && is_array($result['data'])) {
+                                    // Itera sobre cada usuário no array 'data'
+                                    foreach ($result['data'] as $usuario) {
+                                        $email = $usuario["email"];
+                                        
+                                        // Chama o método para enviar email para o usuário atual
+                                        $service->EnviaEmailNotificarVenda($email);
+                                    }
+                            
+                                    http_response_code(200);
+                                    echo json_encode([
+                                        "message" => "Emails enviados com sucesso!"
+                                    ]);
+                                } else {
+                                    // Retorna uma mensagem de erro se 'data' estiver vazio ou for inválido
+                                    http_response_code(404);
+                                    echo json_encode([
+                                        "message" => "Nenhum usuário encontrado para o perfil especificado."
+                                    ]);
+                                }
+                                break;
+                                case $action == "FaltaProduto":
+                                    // Chama o serviço para obter os usuários com o perfil "Administrador"
+                                    $result = $service->readByPerfil2("Administrador");
+                                    $p=  $_GET['produto'];
+                                
+                                    // Verifica se a chave 'data' existe e é um array
+                                    if (isset($result['data']) && is_array($result['data'])) {
+                                        // Itera sobre cada usuário no array 'data'
+                                        foreach ($result['data'] as $usuario) {
+                                            $email = $usuario["email"];
+                                            
+                                            // Chama o método para enviar email para o usuário atual
+                                            $service->EnviaEmailFaltaProduto($email,$p);
+                                        }
+                                
+                                        http_response_code(200);
+                                        echo json_encode([
+                                            "message" => "Emails enviados com sucesso!"
+                                        ]);
+                                    } else {
+                                        // Retorna uma mensagem de erro se 'data' estiver vazio ou for inválido
+                                        http_response_code(404);
+                                        echo json_encode([
+                                            "message" => "Nenhum usuário encontrado para o perfil especificado."
+                                        ]);
+                                    }
+                                    break;
+                                   
             default:
                 $id = isset($_GET['id']) ? $_GET['id'] : null;
                 var_dump("deu ruim");
--- BackendProjetoPaz/Service/UsuarioService.php
+++ BackendProjetoPaz/Service/UsuarioService.php
@@ -111,6 +111,35 @@ class UsuarioService {
         ];
         echo json_encode($response);
     }
+
+    public function readByPerfil2($perfil) {
+        $response = [
+            "data" => null,
+            "message" => "Nenhum usuário encontrado para o perfil."
+        ];
+    
+        if ($perfil) {
+            $result = $this->repository->getUsuariosByPerfil($perfil);
+            
+            // Remove a senha dos resultados
+            foreach ($result as &$usuario) {
+                unset($usuario['senha']);
+            }
+            unset($usuario);
+    
+            $status = !empty($result) ? 200 : 404;
+            http_response_code($status);
+    
+            $response = [
+                "data" => !empty($result) ? $result : null,
+                "message" => !empty($result) ? "Usuários encontrados para o perfil especificado." : "Nenhum usuário encontrado para o perfil."
+            ];
+        }
+    
+        // Retorna o array de resposta ao invés de imprimi-lo
+        return $response;
+    }
+    
         
 
     public function update($data) {
@@ -172,7 +201,7 @@ class UsuarioService {
         $mail->addAddress($email);
         // Conteúdo da mensagem
         $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
-        $mail->Subject = 'Teste Envio de Email usando PHPMailer';
+        $mail->Subject = 'Redefinição de Senha';
         $mail->Body    = '<!DOCTYPE html>
         <html lang="pt-BR">
         <head>
@@ -243,7 +272,7 @@ class UsuarioService {
                 <div class="content">
                     <p>Olá, Usuario estamos aqui para ajudar!</p>
                     <p>Recebemos uma solicitação para redefinir a sua senha. Se você fez essa solicitação, clique no botão abaixo para redefinir sua senha:</p>
-                    <a href="localhost:5327/" class="button">Redefinir Senha</a>
+                    <a href="http://localhost:5173/nova-senha?email='.$email.'" class="button">Redefinir Senha</a>
                     <p>Se você não solicitou a redefinição, pode ignorar este e-mail.</p>
                     <p>Atenciosamente,<br>Equipe de Desenvolvimento</p>
                 </div>
@@ -482,6 +511,246 @@ class UsuarioService {
                 </div>
             </body>
             </html>';
+        $mail->AltBody = 'Enviando parametros para email';
+        // Enviar
+        $mail->send();
+        echo 'A mensagem foi enviada!';
+    }
+    catch (Exception $e)
+    {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+    }
+
+    public function EnviaEmailNotificarVenda($email) {
+        $mail = new PHPMailer(true);
+        try
+        {
+        // Configurações do servidor
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();        //Devine o uso de SMTP no envio
+        $mail->SMTPAuth = true; //Habilita a autenticação SMTP
+        $mail->Username   = 'projectpazinterprise@gmail.com';
+        $mail->Password   = 'onlt mrec jzfx bjbr';
+        // Criptografia do envio SSL também é aceito
+        $mail->SMTPSecure = 'tls';
+        // Informações específicadas pelo Google
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        // Define o remetente
+        $mail->setFrom('projectpazinterprise@gmail.com');
+        // Define o destinatário
+        $mail->addAddress($email);
+        // Conteúdo da mensagem
+        $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+        $mail->Subject = 'Agradecimentos por estar no projeto Paz';
+        $mail->Body    = '<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <style>
+                   /* Estilos gerais */
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color: #f4f4f4;
+                    color: #333333;
+                }
+                .container {
+                    max-width: 600px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #ffffff;
+                    border-radius: 8px;
+                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+                }
+                .header {
+                    text-align: center;
+                    padding: 20px 0;
+                    background-color: #007bff;
+                    color: #ffffff;
+                    border-radius: 8px 8px 0 0;
+                }
+                .header h1 {
+                    margin: 0;
+                    font-size: 24px;
+                }
+                .content {
+                    padding: 20px;
+                    text-align: center;
+                }
+                .content p {
+                    font-size: 16px;
+                    line-height: 1.5;
+                }
+                .button {
+                    display: inline-block;
+                    padding: 12px 24px;
+                    margin: 20px 0;
+                    font-size: 16px;
+                    font-weight: bold;
+                    color: #ffffff;
+                    background-color: #007bff;
+                    text-decoration: none;
+                    border-radius: 5px;
+                }
+                .footer {
+                    text-align: center;
+                    font-size: 12px;
+                    color: #777777;
+                    padding: 10px 20px;
+                    border-top: 1px solid #dddddd;
+                    border-radius: 0 0 8px 8px;
+                }
+            </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Nova Venda Realizada</h1>
+        </div>
+        <div class="content">
+            <p>Olá,</p>
+            <p>Informamos que uma nova venda foi concluída com sucesso em nossa plataforma.</p>
+            <p><strong>Detalhes da Venda:</strong></p>
+            <ul>
+                <li><strong>Vendedor:</strong> Nome do Vendedor</li>
+                <li><strong>Data da Venda:</strong> Data da Venda</li>
+            </ul>
+            <p>Para mais informações e acompanhamento, acesse o painel administrativo.</p>
+            <p>Atenciosamente,<br>Equipe de Desenvolvimento</p>
+        </div>
+        <div class="footer">
+            <p>Este e-mail foi enviado automaticamente. Por favor, não responda.</p>
+            <p>&copy; 2024 PAZ. Todos os direitos reservados.</p>
+        </div>
+    </div>
+</body>
+</html>
+';
+        $mail->AltBody = 'Enviando parametros para email';
+        // Enviar
+        $mail->send();
+        echo 'A mensagem foi enviada!';
+    }
+    catch (Exception $e)
+    {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+    }
+
+    public function EnviaEmailFaltaProduto($email,$Produto) {
+        $mail = new PHPMailer(true);
+
+        $data = new DateTime();
+        var_dump($data);die;
+        try
+        {
+        // Configurações do servidor
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();        //Devine o uso de SMTP no envio
+        $mail->SMTPAuth = true; //Habilita a autenticação SMTP
+        $mail->Username   = 'projectpazinterprise@gmail.com';
+        $mail->Password   = 'onlt mrec jzfx bjbr';
+        // Criptografia do envio SSL também é aceito
+        $mail->SMTPSecure = 'tls';
+        // Informações específicadas pelo Google
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        // Define o remetente
+        $mail->setFrom('projectpazinterprise@gmail.com');
+        // Define o destinatário
+        $mail->addAddress($email);
+        // Conteúdo da mensagem
+        $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+        $mail->Subject = 'Agradecimentos por estar no projeto Paz';
+        $mail->Body    = '<!DOCTYPE html>
+        <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+            /* Estilos gerais */
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color: #f4f4f4;
+                    color: #333333;
+                }
+                .container {
+                    max-width: 600px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #ffffff;
+                    border-radius: 8px;
+                    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+                }
+                .header {
+                    text-align: center;
+                    padding: 20px 0;
+                    background-color: #007bff;
+                    color: #ffffff;
+                    border-radius: 8px 8px 0 0;
+                }
+                .header h1 {
+                    margin: 0;
+                    font-size: 24px;
+                }
+                .content {
+                    padding: 20px;
+                    text-align: center;
+                }
+                .content p {
+                    font-size: 16px;
+                    line-height: 1.5;
+                }
+                .button {
+                    display: inline-block;
+                    padding: 12px 24px;
+                    margin: 20px 0;
+                    font-size: 16px;
+                    font-weight: bold;
+                    color: #ffffff;
+                    background-color: #007bff;
+                    text-decoration: none;
+                    border-radius: 5px;
+                }
+                .footer {
+                    text-align: center;
+                    font-size: 12px;
+                    color: #777777;
+                    padding: 10px 20px;
+                    border-top: 1px solid #dddddd;
+                    border-radius: 0 0 8px 8px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="header">
+                    <h1>Produto em Falta</h1>
+                </div>
+                <div class="content">
+                    <p>Olá,</p>
+                    <p>Informamos que o produto abaixo está indisponível em nossa plataforma.</p>
+                    <p><strong>Detalhes do Produto:</strong></p>
+                    <ul style="text-align: left;">
+                        <li><strong>Nome:</strong> '.$Produto.'</li>
+                        <li><strong>Última Atualização:</strong> '.$data.'</li>
+                    </ul>
+                    <p>Por favor, atualize o estoque ou entre em contato com o fornecedor para restabelecer a disponibilidade do produto o quanto antes.</p>
+                    <p>Atenciosamente,<br>Equipe de Desenvolvimento</p>
+                </div>
+                <div class="footer">
+                    <p>Este e-mail foi enviado automaticamente. Por favor, não responda.</p>
+                    <p>&copy; 2024 PAZ. Todos os direitos reservados.</p>
+                </div>
+            </div>
+        </body>
+        </html>';
         $mail->AltBody = 'Enviando parametros para email';
         // Enviar
         $mail->send();
