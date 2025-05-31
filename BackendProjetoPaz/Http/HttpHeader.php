f67bcc913af299f503896445617da21ad31f43e0
nasc2005 <murilovnasc.prog@gmail.com>
2025-05-31 13:33:20 -0300

    HttpHeader sem cross origin

--- BackendProjetoPaz/Http/HttpHeader.php
+++ BackendProjetoPaz/Http/HttpHeader.php
@@ -2,26 +2,14 @@
 namespace App\Backend\Http;
 
 class HttpHeader {
-    private static $allowedOrigins = [
-        'http://localhost',
-        'http://localhost:5173',
-        'http://localhost:3000',
-    ];
-
     public static function setDefaultHeaders() {
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-
-        if (in_array($origin, self::$allowedOrigins)) {
-            header("Access-Control-Allow-Origin: $origin");
-        } else {
-            header("Access-Control-Allow-Origin: " . self::$allowedOrigins[0]); 
-        }
-
+        $origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+        header("Access-Control-Allow-Origin: $origin");
+        header("Access-Control-Allow-Credentials: true");
         header("Content-Type: application/json; charset=UTF-8");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Access-Control-Max-Age: 3600");
         header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-        header("Access-Control-Allow-Credentials: true"); 
     }
 
     public static function sendNotAllowedMethod() {
@@ -30,3 +18,4 @@ class HttpHeader {
         exit;
     }
 }
+
